c12e3434ac8c65a004c64fcc15f207dae1275c14
buds55471-star <buds55471@gmail.com>
2025-10-27 21:18:34 +0530

    Add files via upload

---
+++ days_1_project.py
@@ -0,0 +1,12 @@
+''' Imagine a little chatbot that:
+Greets you
+Asks for your name
+Asks your age
+Tells you how old you'll be next year '''
+
+print("Hello sir, I am your chatbot")
+name = input("What's your name:")
+age = int(input("What's your age:"))
+
+print(f"Hi, {name} nice to meet you!")
+print(f"Your age will be {age+1} in next years")
